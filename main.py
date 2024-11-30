a32565530195182afa6b4a545860dc6de50ffd2f
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-11-30 16:04:53 +0100

    feat: add more verbose logging

--- main.py
+++ main.py
@@ -194,6 +194,7 @@ async def enable_schedule_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE
     assert context.job_queue
     assert update.effective_chat
     assert update.effective_message
+    assert update.effective_user
 
     chat_id = update.effective_message.chat_id
     if jobs_exist(str(chat_id), context):
@@ -233,15 +234,19 @@ Schedule has been enabled with the following settings:
 
 - Poll will be sent every Sunday at {args.poll_hour}:00.
 - Tomorrow's people message will be sent at {args.tomorrow_message_hour}:00."""
+    logger.info("User %s enabled schedule in chat %s", update.effective_user.id, chat_id)
     await update.effective_chat.send_message(message_text, disable_notification=True)
 
 
 async def disable_schedule_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     assert update.effective_chat
     assert update.effective_message
+    assert update.effective_user
 
-    job_removed = remove_job_if_exists(str(update.effective_message.chat_id), context)
+    chat_id = update.effective_message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
     message_text = "Schedule has been disabled." if job_removed else "Schedule was not enabled."
+    logger.info("User %s disabled schedule in chat %s", update.effective_user.id, chat_id)
     await update.effective_chat.send_message(message_text, disable_notification=True)
 
 
