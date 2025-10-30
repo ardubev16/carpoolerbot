8c73ced570fc83ea8d38fc5a82246bd4cb884909
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-10-30 19:26:18 +0100

    chore: add missing env var to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,4 @@ os.environ["DB_NAME"] = "test_db"
 os.environ["DB_USERNAME"] = "test_user"
 os.environ["DB_PASSWORD"] = "test_password"  # noqa: S105
 os.environ["HOLIDAYS_COUNTRY"] = "US"
+os.environ["HOLIDAYS_SUBDIV"] = ""
