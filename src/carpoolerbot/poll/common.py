d615642957622a65cfbb09dc748f8f1d07c6d90e
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2026-07-19 12:55:49 +0200

    fix: send poll if previous one deleted

--- src/carpoolerbot/poll/common.py
+++ src/carpoolerbot/poll/common.py
@@ -1,13 +1,22 @@
+import logging
+
+import telegram.error
 from telegram import Bot
 
 from carpoolerbot.database import Session
 from carpoolerbot.database.models import WeeklyPoll
 from carpoolerbot.database.repositories.poll import close_poll, get_latest_poll
 
+logger = logging.getLogger(__name__)
+
 
 async def send_poll(bot: Bot, chat_id: int) -> None:
     if latest_poll := get_latest_poll(chat_id):
-        await bot.stop_poll(chat_id, latest_poll.message_id)
+        try:
+            await bot.stop_poll(chat_id, latest_poll.message_id)
+        except telegram.error.BadRequest:
+            logger.warning("Failed to stop poll with message_id %s in chat_id %s", latest_poll.message_id, chat_id)
+
         close_poll(chat_id, latest_poll.message_id)
         await bot.unpin_chat_message(chat_id, latest_poll.message_id)
 
