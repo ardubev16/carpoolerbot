a11a1c78fa155e1dc11a967bbe3438155cb5a3d6
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2026-07-19 13:09:23 +0200

    fix: also try/except unpin

--- src/carpoolerbot/poll/common.py
+++ src/carpoolerbot/poll/common.py
@@ -14,11 +14,15 @@ async def send_poll(bot: Bot, chat_id: int) -> None:
     if latest_poll := get_latest_poll(chat_id):
         try:
             await bot.stop_poll(chat_id, latest_poll.message_id)
+            await bot.unpin_chat_message(chat_id, latest_poll.message_id)
         except telegram.error.BadRequest:
-            logger.warning("Failed to stop poll with message_id %s in chat_id %s", latest_poll.message_id, chat_id)
+            logger.warning(
+                "Failed to stop/unpin poll with message_id %s in chat_id %s",
+                latest_poll.message_id,
+                chat_id,
+            )
 
         close_poll(chat_id, latest_poll.message_id)
-        await bot.unpin_chat_message(chat_id, latest_poll.message_id)
 
     options = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     message = await bot.send_poll(
